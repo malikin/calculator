0da1d19d0f2a7dd26a5b4d35a5f77daced9dbb95
Timur Malikin <timur.malikin@gmail.com>
2017-10-08 19:16:55 +0000

    First app struct and tests

---
+++ src/calculator.erl
@@ -0,0 +1,40 @@
+-module(calculator).
+
+%% API exports
+-export([
+         eval_expression/1,
+         parse_expression/1,
+         validate_input/1
+        ]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+eval_expression(Expression) ->
+  eval_expession_stack(Expression, []).
+
+parse_expression(_Expr) -> ok.
+
+validate_input(String) ->
+  validate_input(String, 0).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+eval_expession_stack({plus, {num, X}, {num, Y}}, _Stack) ->
+  X + Y.
+
+validate_input([], 0) ->
+  {ok, valid};
+
+validate_input([], _) ->
+  {error, braces_error};
+
+validate_input([H|T], Acc) ->
+  case H of
+    40 -> validate_input(T, Acc + 1); % Open brace
+    41 -> validate_input(T, Acc - 1); % Close brace
+    _   -> validate_input(T, Acc)
+  end.
---
+++ test/calculator_eunit.erl
@@ -0,0 +1,12 @@
+-module(calculator_eunit).
+-include_lib("eunit/include/eunit.hrl").
+% -include("../src/calculator.erl").
+-compile(export_all).
+
+eval_expresstion_test() ->
+  ?assertEqual(2, calculator:eval_expression({plus, {num, 1}, {num, 1}})),
+  ?assertEqual(0, calculator:eval_expression({minus, {num, 1}, {num, 1}})).
+
+validate_input_test() ->
+  ?assertEqual({ok, valid}, calculator:validate_input("(1 + 2 - (0 + 1))")),
+  ?assertEqual({error, braces_error}, calculator:validate_input("(1 + 1))")).
