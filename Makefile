9ce4586efe2cef9b5370b55b82c2822baa6ccca3
Timur Malikin <timur.malikin@gmail.com>
2017-10-16 20:13:55 +0000

    Small fixes, added new tests

---
+++ Makefile
@@ -0,0 +1,7 @@
+test:
+	rebar3 eunit
+
+compile:
+	rebar3 compile
+
+.PHONY: test
--- src/calculator.erl
+++ src/calculator.erl
@@ -14,29 +14,52 @@
 eval_expression(Expression) ->
   eval_expession_stack(Expression, []).
 
-parse_expression(_Expr) -> ok.
+parse_expression(Expr) ->
+  parse_expression(Expr, [], []).
 
 validate_input(String) ->
-  validate_input(String, 0).
+  validate_input(String, []).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
+parse_expression([H|T], OperStack, DataStack) ->
+  OpenBrace  = 40,
+  CloseBrace = 41,
+  erlang:display(H),
+  erlang:display(T),
+  case H of
+    OpenBrace  -> parse_expression(T, [OpenBrace|OperStack], DataStack);
+    CloseBrace -> {hd(OperStack), [{num, Num} || Num <- DataStack]};
+    "+" -> parse_expression(T, [plus|OperStack], DataStack);
+    Val -> parse_expression(T, OperStack, [Val, DataStack])
+  end.
+
 eval_expession_stack({plus, {num, X}, {num, Y}}, _Stack) ->
-  X + Y.
+  X + Y;
+
+eval_expession_stack({minus, {num, X}, {num, Y}}, _Stack) ->
+  X - Y.
 
 %% Validate input, check braces input balance
 
-validate_input([], 0) ->
+validate_input([], []) ->
   {ok, valid};
 
-validate_input([], _) ->
+validate_input([], _Stack) ->
   {error, braces_error};
 
-validate_input([H|T], Acc) ->
+validate_input([H|T], Stack) ->
+  OpenBrace  = 40,
+  CloseBrace = 41,
   case H of
-    40 -> validate_input(T, Acc + 1); % Open brace
-    41 -> validate_input(T, Acc - 1); % Close brace
-    _  -> validate_input(T, Acc)
+    OpenBrace  -> validate_input(T, [OpenBrace|Stack]);
+    CloseBrace ->
+      if
+        hd(Stack) =:= OpenBrace -> validate_input(T, tl(Stack));
+        true                    -> {error, braces_error}
+      end;
+    _  -> validate_input(T, Stack)
   end.
+
