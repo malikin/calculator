1ccfacc761d26ab6512eb6423877b0e7f92f052b
Timur Malikin <timur.malikin@billing.ru>
2017-10-17 15:04:49 +0300

    Refactor

--- src/calculator.erl
+++ src/calculator.erl
@@ -9,6 +9,10 @@
 
 -define(open_brace, $().
 -define(close_brace, $)).
+-define(plus, $+).
+-define(minus, $-).
+-define(multiply, $*).
+-define(divide, $/).
 
 %%====================================================================
 %% API functions
@@ -27,32 +31,35 @@ validate_input(String) ->
 %% Internal functions
 %%====================================================================
 
+%% Parse string expression to ast
 parse_expression([H|T], OperStack, DataStack) ->
-  Plus = $+,
-  Minus = $-,
-  Multiply = $*,
-  Divide = $/,
-
   case H of
     ?open_brace ->
       parse_expression(T, [?open_brace|OperStack], DataStack);
     ?close_brace ->
       Operands = [{num, Num} || Num <- DataStack],
       list_to_tuple([hd(OperStack) | Operands]);
-    Plus ->
-      parse_expression(T, [plus|OperStack], DataStack);
-    Minus ->
-      parse_expression(T, [minus|OperStack], DataStack);
-    Multiply ->
-      parse_expression(T, [multiply|OperStack], DataStack);
-    Divide ->
-      parse_expression(T, [divide|OperStack], DataStack);
+    Operator when Operator =:= ?minus;
+                  Operator =:= ?plus;
+                  Operator =:= ?multiply;
+                  Operator =:= ?divide
+                  ->
+      parse_expression(T, [get_operator(Operator)|OperStack], DataStack);
     Val when Val >= $0, Val =< $9 ->
       parse_expression(T, OperStack, [list_to_integer([Val])|DataStack]);
     _ ->
       parse_expression(T, OperStack, DataStack)
   end.
 
+get_operator(Operator) ->
+  case Operator of
+    ?plus -> plus;
+    ?minus -> minus;
+    ?multiply -> multiply;
+    ?divide -> divide
+  end.
+
+%% Eval parsed expressions from ast
 eval_expession_stack({plus, {num, X}, {num, Y}}, _Stack) ->
   X + Y;
 
@@ -60,7 +67,6 @@ eval_expession_stack({minus, {num, X}, {num, Y}}, _Stack) ->
   X - Y.
 
 %% Validate input, check braces input balance
-
 validate_input([], []) ->
   {ok, valid};
 
